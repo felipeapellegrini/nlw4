51d29842e0415094e189b70bdec0156790f608c7
Felipe de Almeida <felipeapellegrini@gmail.com>
2021-02-24 09:26:56 -0300

    fix: pass survey table to entity

--- src/models/Survey.ts
+++ src/models/Survey.ts
@@ -1,7 +1,7 @@
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
 
-@Entity()
+@Entity("survey")
 class Survey {
   @PrimaryColumn()
   readonly id: string;
