0d9e81ccaf83870bec35760970a7751425a6eb91
Felipe de Almeida <felipeapellegrini@gmail.com>
2021-02-25 09:31:08 -0300

    feat: add SurveysUsers repositories

---
+++ src/repositories/SurveysUsers.ts
@@ -0,0 +1,8 @@
+import { Repository } from "typeorm";
+import { SurveyUser } from "../models/SurveyUser";
+
+class SurveysUsersRepository extends Repository<SurveyUser> {
+  //
+}
+
+export { SurveysUsersRepository };
