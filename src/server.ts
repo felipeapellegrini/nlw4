3795aa8b1d058f73d5066a805b23918c9e4af3dd
Felipe de Almeida <felipeapellegrini@gmail.com>
2021-02-24 23:52:42 -0300

    fix: fix error in Entity Metadata

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
+import createConnection from "./database";
 import { app } from "./app";
+
+createConnection();
 
 app.listen(3333, () => console.log("Server is running!")); // eslint-disable-line no-console
 
